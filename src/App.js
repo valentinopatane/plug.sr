aa71840d78fcf6f18de17df089b4cab9c42dc8ba
Valentino Patane <valentinopatane98@gmail.com>
2022-08-24 21:19:13 -0300

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
-import React from 'react';
-import {Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 //CSS Basics
-import './App.css';
+import "./App.css";
 //NAVBAR
-import Navbar from './components/NavBar';
+import Navbar from "./components/NavBar";
 //PAGES
-import Home from './views/Home';
-import Shop from './views/Shop';
-import ItemDetailContainer from './views/ItemDetailContainer';
-import Categories from './views/Categories';
-import Cart from './views/Cart'
-import Checkout from './views/Checkout';
-import Archive from './views/Archive';
-import About from './views/About';
-import Error404 from './views/Error404';
+import Home from "./views/Home";
+import Shop from "./views/Shop";
+import ItemDetailContainer from "./views/ItemDetailContainer";
+import Categories from "./views/Categories";
+import Cart from "./views/Cart";
+import Checkout from "./views/Checkout";
+import Archive from "./views/Archive";
+import About from "./views/About";
+import Error404 from "./views/Error404";
 
 //CONTEXT
-import {ItemsProvider} from './components/context/ItemsContext';
-
-
+import { ItemsProvider } from "./components/context/ItemsContext";
 
 function App() {
-  return (
-    <ItemsProvider>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route exact path={'/plug.sr'} element={<Home/>}/>
-          <Route exact path={'/plug.sr/shop'} element={<Shop/>}/>
-          <Route exact path={'/plug.sr/shop/item/:id'} element={<ItemDetailContainer/>}/>
-          <Route exact path={'/plug.sr/category/:id'} element={<Categories/>}/>
-          <Route exact path={'/plug.sr/cart'} element={<Cart/>}/> 
-          <Route exact path={'/plug.sr/checkout'} element={<Checkout/>}/>
-          <Route exact path={'/plug.sr/archive'} element={<Archive/>}/>
-          <Route exact path={'/plug.sr/about'}element={<About/>}/>
-          <Route path='*' element={<Error404/>}/>
-        </Routes>
-      </Router>
-    </ItemsProvider>
-  );
+    return (
+        <ItemsProvider>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route exact path={"/plug.sr"} element={<Home />} />
+                    <Route exact path={"/plug.sr/shop"} element={<Shop />} />
+                    <Route
+                        exact
+                        path={"/plug.sr/shop/item/:id"}
+                        element={<ItemDetailContainer />}
+                    />
+                    <Route
+                        exact
+                        path={"/plug.sr/shop/category/:id"}
+                        element={<Categories />}
+                    />
+                    <Route exact path={"/plug.sr/cart"} element={<Cart />} />
+                    <Route
+                        exact
+                        path={"/plug.sr/checkout"}
+                        element={<Checkout />}
+                    />
+                    <Route
+                        exact
+                        path={"/plug.sr/archive"}
+                        element={<Archive />}
+                    />
+                    <Route exact path={"/plug.sr/about"} element={<About />} />
+                    <Route path="*" element={<Error404 />} />
+                </Routes>
+            </Router>
+        </ItemsProvider>
+    );
 }
 
 export default App;
-
--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,17 +1,34 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import './style.css';
+import { Link } from "react-router-dom";
+import "./style.css";
 
-const SubMenu = ()=>{
-    return(
-            <div className="subMenu">
-                <ul>
-                    <Link className="subMenu_link" to={"/shop"}>All</Link>
-                    <Link className="subMenu_link" to={"/category/top"}>Top pieces</Link>
-                    <Link className="subMenu_link" to={"/category/pants"}>Pants</Link>
-                    <Link className="subMenu_link" to={"/category/accesory"}>Accesories</Link>
-                </ul>
-            </div>
-    )
-}
-export default SubMenu
+const SubMenu = () => {
+    return (
+        <div className="subMenu">
+            <ul>
+                <Link className="subMenu_link" to={"/plug.sr/shop"}>
+                    All
+                </Link>
+                <Link
+                    className="subMenu_link"
+                    to={"/plug.sr/shop/category/top"}
+                >
+                    Top pieces
+                </Link>
+                <Link
+                    className="subMenu_link"
+                    to={"/plug.sr/shop/category/pants"}
+                >
+                    Pants
+                </Link>
+                <Link
+                    className="subMenu_link"
+                    to={"/plug.sr/shop/category/accesory"}
+                >
+                    Accesories
+                </Link>
+            </ul>
+        </div>
+    );
+};
+export default SubMenu;
